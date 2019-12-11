351bce7025b1de94a44d57d83073803f6bedd212
Aveta Vos <avetavos@gmail.com>
2019-12-11 21:20:50 +0700

    Add template engine and set static asset folder

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,45 @@
-import express, { Application } from 'express';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import mongoose, { mongo } from 'mongoose';
 import 'dotenv/config';
 
 export default class App {
-	public readonly app: Application;
+	public app: express.Application;
 	constructor() {
 		this.app = express();
-		this.initialMiddlewares();
-		this.initialRoutes();
+		this.initializeDatabase();
+		this.initializeMiddlewares();
+		this.initializeControllers();
+		this.initializeAssets();
+		this.initializeTemplate();
 	}
 
-	private initialMiddlewares() {
+	private initializeMiddlewares(): void {
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true }));
+		this.app.use(cookieParser());
+	}
+
+	private initializeAssets(): void {
+		this.app.use('/', express.static('public'));
+		this.app.use(express.static('views'));
+	}
+
+	private initializeTemplate(): void {
 		this.app.set('view engine', 'pug');
 	}
 
-	private initialRoutes() {
-		this.app.get('/', (req, res) => res.send('Hello World'));
+	private initializeControllers(): void {
+		this.app.get('/', (req, res) => res.render('index'));
+	}
+
+	private async initializeDatabase(): Promise<void> {
+		const { DB_HOST, DB_USER, DB_PASSWORD } = process.env;
+		await mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}${DB_HOST}`, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+		console.log(`Database has connected`);
 	}
 
 	public listen(): void {
